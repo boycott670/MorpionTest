8944944ef2c84b0ec088cfaab0a6749c56b2bc79
takingsss12 <marsouf224@gmail.com>
2018-06-15 12:07:44 +0000

    refactor#1 [refactored src/main/java/sqli/morpion/parsers/MorpionParser.java]

--- src/main/java/sqli/morpion/parsers/MorpionParser.java
+++ src/main/java/sqli/morpion/parsers/MorpionParser.java
@@ -1,39 +1,30 @@
 package sqli.morpion.parsers;
 
+import java.util.function.Function;
 
 import sqli.morpion.entities.Pair;
 
-public class MorpionParser {
-    public Pair<Integer, Integer> morpionParser(String morpionDimension, String separator) {
+public class MorpionParser
+{
+	private static <F, S> Pair<F, S> parse(final String haystack, final String delimitor, final Function<? super String, ? extends F> firstTokenParser, final Function<? super String, ? extends S> secondTokenParser)
+	{
+		final String[] tokens = haystack.split(delimitor);
+		
+		return new Pair<>(firstTokenParser.apply(tokens[0]), secondTokenParser.apply(tokens[1]));
+	}
+	
+	public Pair<Integer, Integer> morpionParser(String morpionDimension, String separator)
+	{
+		return parse(morpionDimension, separator, Integer::parseInt, Integer::parseInt);
+	}
 
-        Integer line    = Integer.parseInt(morpionDimension.split(separator)[0]);
+	public Pair<String, String> playerParser(String player, String separator)
+	{
+		return parse(player, separator, Function.identity(), Function.identity());
+	}
 
-        Integer column  = Integer.parseInt(morpionDimension.split(separator)[1]);
-
-        return new Pair<>(line , column);
-
-    }
-
-
-
-    public Pair<String, String> playerParser(String player, String separator) {
-
-        String  playerName    = player.split(separator)[0];
-
-        String  character     = player.split(separator)[1];
-
-        return new Pair<>(playerName , character);
-
-    }
-    public Pair<Integer, Integer> positionParser(String postion, String separator) {
-
-        Integer  playerName    =Integer.parseInt(postion.split(separator)[0]);
-
-        Integer  character     = Integer.parseInt(postion.split(separator)[1]);
-
-        return new Pair<>(playerName , character);
-
-    }
-
-
+	public Pair<Integer, Integer> positionParser(String postion, String separator)
+	{
+		return morpionParser(postion, separator);		
+	}
 }
